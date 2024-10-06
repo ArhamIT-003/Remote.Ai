4b397446727079c0c5f38e638014bda9ad369bdd
Arham <muhammadarham59@gmail.com>
2024-10-06 09:51:10 +0500

    Changes made

--- UI/Server/controllers/Upload-Controller.js
+++
@@ -1,8 +0,0 @@
-import { imagekit } from "../utils.js";
-
-export const ImageKitAuthenticator = async (req, res) => {
-  let result = imagekit.getAuthenticationParameters();
-  res.send(result);
-};
-
-export const UploadFileHandler = async (req, res) => {};
